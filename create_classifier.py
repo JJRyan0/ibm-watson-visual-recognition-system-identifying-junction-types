8f01a94aa269f638f8b4bf60ea7c0b56a657185a
John Ryan <johnjoseph.ryan0@gmail.com>
2021-06-23 14:39:55 +0100

    Update create_classifier.py

--- create_classifier.py
+++ create_classifier.py
@@ -5,7 +5,7 @@ from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
 
-visual_recognition = VisualRecognitionV3(VisualRecognitionV3.latest_version, api_key='ebea3d6c70451937afbf1a5c8dd176b5b2b613cb')
+visual_recognition = VisualRecognitionV3(VisualRecognitionV3.latest_version, api_key='ebxxxxxx')
 
 with open(join(dirname(__file__), 'unmarkedjunctions.zip'), 'rb') as unmarked, \
     open(join(dirname(__file__), 'markedjunctions.zip'), 'rb') as marked, \
